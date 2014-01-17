9e0063bddcc064188bb86ec9156a89e228b6ac7f
nvl <nvl@redcom.ru>
2014-01-17 11:02:05 +1100

    start config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Registrator::Application.config.secret_key_base = 'f173b79853a244847fc037c40cf397e7949b19a6fe9ee7c679be4e1de2bfff959f688d76309395d587c8cdf52579155fd55ecd1b1694741fb5ca0dd39ac89f41'
