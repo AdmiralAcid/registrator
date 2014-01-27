4348a9c6fe566586d88cfe9a79c18c9e33246f76
AdmiralAcid <coldcore@mail.ru>
2014-01-27 12:28:45 +1100

    adding tests for another 2 tables

--- test/models/apply_test.rb
+++ test/models/apply_test.rb
@@ -1,7 +1,125 @@
 require 'test_helper'
 
 class ApplyTest < ActiveSupport::TestCase
+	fixtures :applies
   # test "the truth" do
   #   assert true
   # end
+  def new_phonestatus(phoneApplyStatus)
+  	Apply.new(
+  		applyNumber: 292,
+  		nameOrFIO: "Айдол Уильям",
+  		street: "ул.Ленина", 
+			house: 88,
+			phoneApplyStatus: phoneApplyStatus,
+			applyTreatedBy: "Омен Екатерина Продиджевна",
+			phoneContractConcludedBy: "Омен Екатерина Продиджевна",
+			internetContractConcludedBy: "Омен Екатерина Продиджевна",
+			iptvContractConcludedBy: "Омен Екатерина Продиджевна",
+			clientType: "Юр лицо", 
+			insertionDate: "2013-01-24")
+  end
+
+  def new_netstatus(internetApplyStatus)
+  	Apply.new(
+  		applyNumber: 292,
+  		nameOrFIO: "Айдол Уильям",
+  		street: "ул.Ленина", 
+			house: 88,
+			internetApplyStatus: internetApplyStatus,
+			applyTreatedBy: "Омен Екатерина Продиджевна",
+			phoneContractConcludedBy: "Омен Екатерина Продиджевна",
+			internetContractConcludedBy: "Омен Екатерина Продиджевна",
+			iptvContractConcludedBy: "Омен Екатерина Продиджевна",
+			clientType: "Юр лицо", 
+			insertionDate: "2013-01-24")
+  end
+
+  def new_iptvstatus(iptvApplyStatus)
+  	Apply.new(
+  		applyNumber: 292,
+  		nameOrFIO: "Айдол Уильям",
+  		street: "ул.Ленина", 
+			house: 88,
+			iptvApplyStatus: iptvApplyStatus,
+			applyTreatedBy: "Омен Екатерина Продиджевна",
+			phoneContractConcludedBy: "Омен Екатерина Продиджевна",
+			internetContractConcludedBy: "Омен Екатерина Продиджевна",
+			iptvContractConcludedBy: "Омен Екатерина Продиджевна",
+			clientType: "Юр лицо", 
+			insertionDate: "2013-01-24")
+  end
+
+  def new_clienttype(clientType)
+  	Apply.new(
+  		applyNumber: 292,
+  		nameOrFIO: "Айдол Уильям",
+  		street: "ул.Ленина", 
+			house: 88,
+			applyTreatedBy: "Омен Екатерина Продиджевна",
+			phoneContractConcludedBy: "Омен Екатерина Продиджевна",
+			internetContractConcludedBy: "Омен Екатерина Продиджевна",
+			iptvContractConcludedBy: "Омен Екатерина Продиджевна",
+			clientType: clientType, 
+			insertionDate: "2013-01-24")
+  end
+
+  test "checking phone apply status to be right" do
+  	bad = ["инсотолляция", "Атказ", "Взятка"]
+  	bad.each do |name|
+  		assert new_phonestatus(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  	right = ["Установка", "Проверка", "В работе", "Отказ", "Отложено", "Очередь"]
+  	right.each do |name|
+  		assert new_phonestatus(name).valid?, "#{name} should be valid"
+  	end
+  end
+
+  test "checking internet apply status to be right" do
+  	bad = ["инсотолляция", "Атказ", "Взятка"]
+  	bad.each do |name|
+  		assert new_netstatus(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  	right = ["Установка", "Проверка", "В работе", "Отказ", "Отложено", "Очередь"]
+  	right.each do |name|
+  		assert new_netstatus(name).valid?, "#{name} should be valid"
+  	end
+  end
+
+  test "checking iptv apply status to be right" do
+  	bad = ["инсотолляция", "Атказ", "Взятка"]
+  	bad.each do |name|
+  		assert new_iptvstatus(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  	right = ["Установка", "Проверка", "В работе", "Отказ", "Отложено", "Очередь"]
+  	right.each do |name|
+  		assert new_iptvstatus(name).valid?, "#{name} should be valid"
+  	end
+  end
+
+  test "checking client type to be right" do
+  	bad = ["юрлицо", "Физ. Лицо", "нежилое помещение в физлице"]
+  	bad.each do |name|
+  		assert new_clienttype(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  	right = ["Юр лицо", "Физ лицо", "Физ лицо в нежилом помещении"]
+  	right.each do |name|
+  		assert new_clienttype(name).valid?, "#{name} should be valid"
+  	end
+  end
+
+  test "checking unique apply number" do
+  	apply = Apply.new(
+  		applyNumber: applies(:another_apply).applyNumber,
+  		nameOrFIO: "Айдол Уильям",
+  		street: "ул.Ленина", 
+			house: 88,
+			applyTreatedBy: "Омен Екатерина Продиджевна",
+			phoneContractConcludedBy: "Омен Екатерина Продиджевна",
+			internetContractConcludedBy: "Омен Екатерина Продиджевна",
+			iptvContractConcludedBy: "Омен Екатерина Продиджевна",
+			clientType: "Юр лицо", 
+			insertionDate: "2013-01-24")
+  	assert apply.invalid?
+  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
+	fixtures :clients
   # test "the truth" do
   #   assert true
   # end
+  def new_persontype(personType)
+  	Client.new(
+  		nameOrFIO: "Айдол Уильям",
+			personType: personType,
+			contractAcceptor: "Васильев Василий Васильевич")
+  end
+
+  test "checking person type to be right" do
+  	bad = ["физлицо", "юрлецо", "юрец"]
+  	bad.each do |name|
+  		assert new_persontype(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  	right = ["Юр лицо", "Физ лицо", "Корп", "Другой"]
+  	right.each do |name|
+  		assert new_persontype(name).valid?, "#{name} should be valid"
+  	end
+  end
+
+  test "checking name or FIO to be right" do
+  	client = Client.new(
+  		nameOrFIO: clients(:another_client).nameOrFIO,
+			personType: "Корп",
+			contractAcceptor: "Васильев Василий Васильевич")
+  	assert client.invalid?
+  end
 end
